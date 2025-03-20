1a156010b2ba2ef3234e20b483632e0748f081a9
Om Nema <134232774+omnema007@users.noreply.github.com>
2025-03-20 15:20:22 +0530

    [Om] Add. Quiz Application

--- quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -3,6 +3,8 @@ package com.example.quizapp.controller;
 import com.example.quizapp.Question;
 import com.example.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +16,22 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions() {
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 
     @DeleteMapping("delete/{id}")
-    public String deleteQuestionById(@PathVariable Integer id) {
+    public ResponseEntity<String> deleteQuestionById(@PathVariable Integer id) {
         return questionService.deleteQuestion(id);
     }
 
--- quizapp/src/main/java/com/example/quizapp/service/QuestionService.java
+++ quizapp/src/main/java/com/example/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.example.quizapp.service;
 import com.example.quizapp.Question;
 import com.example.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,21 +15,33 @@ public class QuestionService {
 
     @Autowired
     QuestionDao questionDao;
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
-        return "success";
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 
-    public String deleteQuestion(Integer id) {
+    public ResponseEntity<String> deleteQuestion(Integer id) {
         questionDao.deleteById(id);
-        return "delete successfully";
+        return new ResponseEntity<>("delete successfully", HttpStatus.OK);
     }
 }
